87b77c69f8719795a5b588d2704a4cfb06f7976e
cartoon1110@hanmail.net <cartoon1110@hanmail.net>
2018-12-26 12:00:59 +0900

    test

--- lifecycleScripts/clean.js
+++ lifecycleScripts/clean.js
@@ -3,3 +3,28 @@ var path = require("path");
 
 var location = path.join(__dirname, "..");
 cleanForPublish(location);
+var cleanForPublish = require("clean-for-publish");
+var path = require("path");
+
+var location = path.join(__dirname, "..");
+cleanForPublish(location);
+var cleanForPublish = require("clean-for-publish");
+var path = require("path");
+
+var location = path.join(__dirname, "..");
+cleanForPublish(location);
+var cleanForPublish = require("clean-for-publish");
+var path = require("path");
+
+var location = path.join(__dirname, "..");
+cleanForPublish(location);
+var cleanForPublish = require("clean-for-publish");
+var path = require("path");
+
+var location = path.join(__dirname, "..");
+cleanForPublish(location);
+var cleanForPublish = require("clean-for-publish");
+var path = require("path");
+
+var location = path.join(__dirname, "..");
+cleanForPublish(location);
